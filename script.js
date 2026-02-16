dd8dd7c5b2a1e95016a09fa02729c8288af95a43
BDGio-o <giojmart22@gmail.com>
2026-02-16 14:02:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -25,4 +25,13 @@ cards.forEach(card => {
         card.classList.toggle('flipped');
     });
 });
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
 ``
+
